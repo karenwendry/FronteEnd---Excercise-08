2573f05821f88b444804bec10c5ed258d73b832f
Karen Wendry <s22310244@student.unklab.ac.id>
2025-11-03 13:41:57 +0700

    push 1

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import HelloWorld from "../HelloWorld";
+
+const App = () => {
+  let hello = "Hello World";
+  return (
+    <>
+      <div className="text-[25px] bg-blue-500 m-10 p-10 font-serif">
+        {hello}
+      </div>
+      <HelloWorld/>
+    </>
+  );
+};
+
+
+export default App;
